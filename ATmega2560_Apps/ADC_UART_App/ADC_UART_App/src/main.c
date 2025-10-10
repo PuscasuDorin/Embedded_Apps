5cdf1a1fd3e965fa3679bf0d4607744755d61fda
Puscasu Dorin <puscasudorin20052@gmail.com>
2025-10-10 23:39:46 +0300

    feat: sending messages via UART with the voltage level

--- ATmega2560_Apps/ADC_UART_App/ADC_UART_App/src/main.c
+++ ATmega2560_Apps/ADC_UART_App/ADC_UART_App/src/main.c
@@ -4,8 +4,10 @@
  * Created: 10/10/2025 11:16:19 PM
  * Author : Puscasu Dorin-Vasile
  */ 
+#define F_CPU 16000000UL
 
 #include <avr/io.h>
+#include <util/delay.h>
 #include "ADC.h"
 #include "UART.h"
 
@@ -16,6 +18,19 @@ int main(void)
 	
     while (1) 
     {
+		float ADC_voltage_val = ADC_read_voltage(0, 5);
+		
+		if(ADC_voltage_val < 1.5){
+			UART_TransmitString("Voltage is LOW\n\r");
+		}
+		else if(ADC_voltage_val >= 1.5 && ADC_voltage_val < 3.5){
+			UART_TransmitString("Voltage is NORMAL\n\r");
+		}
+		else if(ADC_voltage_val >= 3.5){
+			UART_TransmitString("Voltage is HIGH\n\r");
+		}
+		
+		_delay_ms(1000);
     }
 }
 
